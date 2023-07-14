880fc5aaf09d513ccd26a9619021b44adb8ddbd0
chrloch <christian.loch@gmail.com>
2023-07-14 16:12:29 +0200

    update test scripts

--- scripts/tests/common.py
+++ scripts/tests/common.py
@@ -2,6 +2,9 @@ import functools
 import pytest
 import os 
 from cryptography import x509
+from base64 import urlsafe_b64decode
+import json
+from warnings import warn
 
 def requires_readable_cert(func):
     'Decorator for tests that should be skipped if the cert has not been loaded'
@@ -13,8 +16,61 @@ def requires_readable_cert(func):
             return func(cert,*args, **kwargs)
     return wrapper
 
+def warn_in_sync_mode(func):
+    '''Decorator for tests that should only yield a warning in sync mode
+      (only works for tests that have a cert parameter)'''
+    @functools.wraps(func)
+    def wrapper(cert, *args, **kwargs):
+        # Step 1: Get SECRETS variable as JSON
+        try: 
+            secrets = json.loads(os.environ.get('SECRETS'), strict=False)
+        except: # Assume secrets is empty if it cannot be loaded
+            secrets = {}
+
+        
+        # Step 2: Check if country of cert has a sync flag that is True
+        country = cert.pathinfo.get('country')
+        if country in secrets \
+            and _padded_b64_json(secrets.get(country)).get('sync'): 
+                sync_flag = True
+        else: 
+                sync_flag = False
+
+        # Step 3: Run the test and catch any exception
+        #         if no exception is raised, the test is passed
+        try:
+            return func(cert,*args, **kwargs)
+        # Step 4: Test is failed
+        #         If sync_flag is True, just raise a warning
+        #         else raise the original exception
+        except AssertionError as ae: 
+            if sync_flag: 
+                warn(" ".join(ae.args),stacklevel=2)
+            else:
+                raise ae
+        except Exception as e: 
+            if sync_flag:
+                warn(f"{e}", source=e)
+            else:
+                raise e
+    return wrapper
+
+      
+
+@functools.lru_cache(maxsize=64)
+def _padded_b64_json(envvalue):
+    '''Corrects the padding of a b64_url encoded string and 
+        then decodes it. Returns empty dict on error.
+    '''
+    try:
+        while len(envvalue) % 4 != 0:
+            envvalue += '='
+        return json.loads(urlsafe_b64decode(envvalue),strict=False)
+    except:
+        return {}
+
 @functools.lru_cache(maxsize=128)
-def collect_onboarding_files(country_folder):
+def collect_onboarding_files(country_folder, convert_upper=False):
     '''  Create a dict of tuples for all found files:
          The key is the domain (DCC, ICAO, etc.)
          Each tuple contains the path starting from the domain folder, 
@@ -24,7 +80,10 @@ def collect_onboarding_files(country_folder):
     onboardings = {}
     for path, dirs, files in os.walk(country_folder):
         for file in files: 
-            long_path = tuple(os.path.join(path, file).split(os.sep)[offset:])
+            fpath = os.path.join(path, file)
+            if convert_upper:
+                fpath = fpath.upper()
+            long_path = tuple(fpath.split(os.sep)[offset:])
             # If we're not scanning country folders but directly an onboarding folder
             if country_folder==f'.{os.sep}onboarding':
                 long_path = ('onboarding',)+long_path
@@ -32,6 +91,9 @@ def collect_onboarding_files(country_folder):
             # Ignore other folders than 'onboarding'
             if long_path[0].lower() == 'onboarding':
                 domain = long_path[1]
+                if domain.startswith('.'):
+                    continue # Allow folders like .git, files like .gitignore, etc. to be 
+                             # present without being seen as onboarding files
                 if not domain in onboardings:
                     onboardings[domain] = []
                 onboardings[domain].append(long_path[2:])
