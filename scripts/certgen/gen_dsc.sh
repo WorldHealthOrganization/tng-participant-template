76ae2ae963944d0ef5defe680825ef655819e5fa
Andreas Scheibal <Andreas.Scheibal@t-systems.com>
2023-10-25 12:32:27 +0200

    feat: generate DSC Genrates a DSC for an SCA in the given subdirectory

--- scripts/certgen/gen_dsc.sh
+++ scripts/certgen/gen_dsc.sh
@@ -8,8 +8,9 @@ subdir=$1
 
 DAYS_DSC=730
 
-openssl req -newkey ec:<(openssl ecparam -name prime256v1) -keyout ${subdir}/DSC.key -nodes -out ${subdir}/DSC_csr.pem -config dsc.conf
-openssl x509 -req -in ${subdir}/DSC_csr.pem -CA ${subdir}/SCA.pem -CAkey ${subdir}/SCA.key -CAcreateserial -days ${DAYS_DSC} -extensions ext -extfile dsc.conf -out ${subdir}/DSC.pem
+#openssl req -newkey ec:<(openssl ecparam -name prime256v1) -keyout ${subdir}/DSC.key -nodes -out ${subdir}/DSC_csr.pem -config DSC.conf  # this does only work on Linux/MacOS
+openssl req -new -newkey ec -pkeyopt ec_paramgen_curve:prime256v1 -keyout ${subdir}/DSC.key -nodes -out ${subdir}/DSC_csr.pem -config DSC.conf
+openssl x509 -req -in ${subdir}/DSC_csr.pem -CA ${subdir}/SCA.pem -CAkey ${subdir}/SCA.key -CAcreateserial -days ${DAYS_DSC} -extensions ext -extfile DSC.conf -out ${subdir}/DSC.pem
 # cleanup
 rm ${subdir}/DSC_csr.pem
 
