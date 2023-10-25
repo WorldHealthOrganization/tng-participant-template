8f57041831ddc7e08da90c4a7698a60cf250ead1
Andreas Scheibal <Andreas.Scheibal@t-systems.com>
2023-10-25 12:16:06 +0200

    feat: generate DSC Genrates a DSC for an SCA in the given subdirectory

---
+++ scripts/certgen/gen_dsc.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <subdir> (which must exist)"
+    exit 1
+fi
+subdir=$1
+
+DAYS_DSC=730
+
+openssl req -newkey ec:<(openssl ecparam -name prime256v1) -keyout ${subdir}/DSC.key -nodes -out ${subdir}/DSC_csr.pem -config dsc.conf
+openssl x509 -req -in ${subdir}/DSC_csr.pem -CA ${subdir}/SCA.pem -CAkey ${subdir}/SCA.key -CAcreateserial -days ${DAYS_DSC} -extensions ext -extfile dsc.conf -out ${subdir}/DSC.pem
+# cleanup
+rm ${subdir}/DSC_csr.pem
+
+
