4b75fbf71acbbb2576741fea76894fbd309c26c7
A200019059 <kaushal.kothari@t-systems.com>
2025-01-22 18:37:12 +0530

    keep only test cases files

--- scripts/createPR.sh
+++
@@ -1,4 +0,0 @@
-export GITHUB_TOKEN=$BOT_TOKEN_GITHUB
-
-gh pr create -B main -H $1/onboardingRequest --title 'Bot requested a change for '$1'.' --body 'Please merge the onnboarding request of '$1'.' > /dev/null 2>&1
-                
